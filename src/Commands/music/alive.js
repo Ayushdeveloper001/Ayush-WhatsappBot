2bf10c77f5a2132d3948a8cc2035491d81367a4d
Ayush Pandey <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-08-09 20:34:28 +0530

    Update alive.js

--- src/Commands/music/alive.js
+++ src/Commands/music/alive.js
@@ -6,7 +6,7 @@ module.exports = {
     description: 'Testing stuff',
     async execute(client, flag, arg, M) {
         M.reply(
-            `Everything is working ${
+            `Everything is working and join our official group https://chat.whatsapp.com/CNZuzvItDaDERDQgGqSz4M ${
                 (await client.contact.getContact(M.sender, client)).username
             } | *Exp:* ${await client.exp.get(M.sender)} *Level:* ${(await client.DB.get(`${M.sender}_LEVEL`)) || 0}`
         )
